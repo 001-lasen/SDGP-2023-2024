39b29078dfa5ee763455b2b7e86e97b7e7ff8c19
Herath Nethmini <senukn2001@gmail.com>
2024-03-06 20:18:47 +0530

    4th commit - created hairstyle pages of male and female
    Created hairstyle page for male category.
    Created hairstyle page for female category.

--- fit_me/lib/mensHairStyleCat.dart
+++ fit_me/lib/mensHairStyleCat.dart
@@ -1,16 +1,78 @@
 import 'package:flutter/material.dart';
 
-class MensHairStyleCat extends StatelessWidget {
+class   MensHairStyleCat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Men\'s Hairstyles'),
-        backgroundColor: Color.fromARGB(255, 238, 2, 187),
+        //title: Text('Men\'s Clothing'),
+        //backgroundColor: Color.fromARGB(255, 238, 2, 187),
       ),
-      body: Center(
-        child: Text('Welcome to the mens\'s hairstyle categories! Currently under development'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _categoryButton("Crew Cut Fade", "imgs/crew_ cut_fade.jpeg"),
+              SizedBox(height: 15), 
+              _categoryButton("Taper Cut", "imgs/taper_cut.jpeg"),
+              SizedBox(height: 15), 
+              _categoryButton("Spiky Hair", "imgs/spiky_hair.jpeg"),
+              SizedBox(height: 15), 
+              _categoryButton("Quiff Hair", "imgs/quiff.jpeg"),
+              SizedBox(height: 15), 
+              _categoryButton("Messy Curls", "imgs/messy_curls.jpeg"),
+              SizedBox(height: 1), 
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  Widget _categoryButton(String categoryName, String imagePath) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          // Handle button press
+        },
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(10.0),
+          backgroundColor: Colors.white, // Set button background color
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Center(
+                child: Text(
+                  categoryName,
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(10.0),
+                image: DecorationImage(
+                  image: AssetImage(imagePath),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: MensHairStyleCat(),
+  ));
 }
--- fit_me/lib/womensHairStyleCat.dart
+++ fit_me/lib/womensHairStyleCat.dart
@@ -5,12 +5,74 @@ class WomesHairStyleCat extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Women\'s Hairstyles'),
-        backgroundColor: Color.fromARGB(255, 238, 2, 187),
+        //title: Text('Men\'s Clothing'),
+        //backgroundColor: Color.fromARGB(255, 238, 2, 187),
       ),
-      body: Center(
-        child: Text('Welcome to the women\'s hairstyle category! Currently under development'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _categoryButton("Curly Hair", "imgs/curly_hair.jpeg"),
+              SizedBox(height: 15), 
+              _categoryButton("Straight Hair", "imgs/straight_hair.jpeg"),
+              SizedBox(height: 15), 
+              _categoryButton("Wavy Hair", "imgs/wavy_hair.jpeg"),
+              SizedBox(height: 15), 
+              _categoryButton("Kinky Hair", "imgs/kinky_hair.jpeg"),
+              SizedBox(height: 15), 
+              _categoryButton("Butterfly Layer", "imgs/butterfly_layer.jpeg"),
+              SizedBox(height: 1), 
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  Widget _categoryButton(String categoryName, String imagePath) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          // Handle button press
+        },
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(10.0),
+          backgroundColor: Colors.white, // Set button background color
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Center(
+                child: Text(
+                  categoryName,
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(10.0),
+                image: DecorationImage(
+                  image: AssetImage(imagePath),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: WomesHairStyleCat(),
+  ));
 }
