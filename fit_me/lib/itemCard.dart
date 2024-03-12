1c0eeb9f0604b6ea3a20b3e4b03f28f30eea60f2
Lasen Punyawardana <lpunyawardana@gmail.com>
2024-03-12 09:45:08 +0530

    6th Commit - item card created
    Created item card for mens hairstyles only

--- fit_me/lib/itemCard.dart
+++ fit_me/lib/itemCard.dart
@@ -1,53 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'camera_screen.dart'; // Import the CameraScreen widget
 
-class ItemCard extends StatelessWidget {
+class ItemCard extends StatefulWidget {
   final String name;
   final String imagePath;
 
   ItemCard({required this.name, required this.imagePath});
 
   @override
+  _ItemCardState createState() => _ItemCardState();
+}
+
+class _ItemCardState extends State<ItemCard> {
+  late CameraController controller;
+  List<CameraDescription> cameras = [];
+
+  @override
+  void initState() {
+    super.initState();
+    availableCameras().then((availableCameras) {
+      cameras = availableCameras;
+      if (cameras.length > 0) {
+        controller = CameraController(cameras[0], ResolutionPreset.medium);
+        controller.initialize().then((_) {
+          if (!mounted) {
+            return;
+          }
+          setState(() {});
+        });
+      }
+    }).catchError((err) {
+      print('Error: $err.code\nError Message: $err.message');
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(name),
+        title: Text(widget.name),
         backgroundColor: Color.fromARGB(255, 238, 2, 187),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            SizedBox(height: 20),
             Container(
-              padding: EdgeInsets.all(10), // Add some padding
+              padding: EdgeInsets.all(10),
               decoration: BoxDecoration(
-                color: Color.fromARGB(255, 238, 2, 187), // Set the background color of the box
-                border: Border.all(color: Color.fromARGB(255, 238, 2, 187)), // Add border
-                borderRadius: BorderRadius.circular(10), // Add border radius
+                color: Color.fromARGB(255, 238, 2, 187),
+                border: Border.all(color: Color.fromARGB(255, 238, 2, 187)),
+                borderRadius: BorderRadius.circular(10),
               ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Image.asset(imagePath), // Display the image
-                  SizedBox(height: 10), // Add some space between the image and the name
-                  Text(name, style: TextStyle(fontSize: 24, color: Colors.white)), // Display the name
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Image.asset(widget.imagePath),
+                  ),
+                  SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      widget.name,
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Description goes here", style: TextStyle(fontSize: 18, color: Colors.white)),
+                  ),
                 ],
               ),
             ),
-            SizedBox(height: 20), // Add some space between the box and the buttons
-            ButtonTheme(
-              minWidth: 200.0,
-              height: 100.0,
+            SizedBox(height: 20),
+            Container(
+              width: 200.0,
+              height: 50.0,
               child: ElevatedButton(
                 onPressed: () {
-                  // Add your code here
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CameraScreen(controller: controller),
+                    ),
+                  );
                 },
                 child: Text('Try On'),
               ),
             ),
-            SizedBox(height: 10), // Add some space between the buttons
-            ButtonTheme(
-              minWidth: 200.0,
-              height: 100.0,
+            SizedBox(height: 10),
+            Container(
+              width: 200.0,
+              height: 50.0,
               child: ElevatedButton(
                 onPressed: () {
                   // Add your code here
