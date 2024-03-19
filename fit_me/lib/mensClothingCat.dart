1fd0b0fdc679ca829b7277e7dcced899e80ac0c2
Minel Perera <minelperera01@gmail.com>
2024-03-19 12:24:47 +0530

    9th commit
    add search bar for men and women categories.

--- fit_me/lib/mensClothingCat.dart
+++ fit_me/lib/mensClothingCat.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 
-class MensClothingCat extends StatelessWidget {
+class MensClothingCat extends StatefulWidget {
+  @override
+  _MensClothingCatState createState() => _MensClothingCatState();
+}
+
+class _MensClothingCatState extends State<MensClothingCat> {
+  String searchText = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Men\'s Clothing'),
         backgroundColor: Color.fromARGB(255, 238, 2, 187),
+        flexibleSpace: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8, // Adjust the width of the search bar
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          searchText = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Search categories...',
+                        border: InputBorder.none,
+                        icon: Icon(Icons.search, size: 20), // Adjust the size of the search icon
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       body: SingleChildScrollView(
         child: Center(
@@ -14,10 +51,15 @@ class MensClothingCat extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               _categoryButton("Hoodies", "imgs/hoodies.jpeg"),
+              SizedBox(height: 15),
               _categoryButton("Trousers", "imgs/trousers.jpeg"),
+              SizedBox(height: 15),
               _categoryButton("Shirts", "imgs/shirts.jpeg"),
+              SizedBox(height: 15),
               _categoryButton("Jackets", "imgs/jackets.jpeg"),
+              SizedBox(height: 15),
               _categoryButton("Suits", "imgs/suits.jpeg"),
+              SizedBox(height: 15),
             ],
           ),
         ),
@@ -37,20 +79,31 @@ class MensClothingCat extends StatelessWidget {
           backgroundColor: Colors.white, // Set button background color
         ),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween, // Align children to start and end
+          crossAxisAlignment: CrossAxisAlignment.center, // Align children vertically centered
           children: [
+            Expanded(
+              child: Center(
+                child: Text(
+                  categoryName,
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+            ),
             Image.asset(
               imagePath,
               width: 100, // Adjust image width as needed
               height: 100, // Adjust image height as needed
-            ),
-            SizedBox(width: 100), // Add some spacing between image and text
-            Text(
-              categoryName,
-              style: TextStyle(fontSize: 20, color: Colors.black),
             ),
           ],
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: MensClothingCat(),
+  ));
 }
--- fit_me/lib/womenClothingCat.dart
+++ fit_me/lib/womenClothingCat.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 
-class WomenensClothingCat extends StatelessWidget {
+class WomenensClothingCat extends StatefulWidget {
+  @override
+  _WomenensClothingCatState createState() => _WomenensClothingCatState();
+}
+
+class _WomenensClothingCatState extends State<WomenensClothingCat> {
+  String searchText = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Women\'s Clothing'),
         backgroundColor: Color.fromARGB(255, 238, 2, 187),
+        flexibleSpace: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.5, // Adjust the width of the search bar
+                    padding: EdgeInsets.symmetric(horizontal:15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: TextField(
+                      onChanged: (value) {
+                        setState(() {
+                          searchText = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Search categories...',
+                        border: InputBorder.none,
+                        icon: Icon(Icons.search, size: 20), // Adjust the size of the search icon
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       body: SingleChildScrollView(
         child: Center(
@@ -14,10 +51,15 @@ class WomenensClothingCat extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               _categoryButton("Blouses", "imgs/blouse.jpeg"),
+              SizedBox(height: 15),
               _categoryButton("Denims", "imgs/denim.jpeg"),
+              SizedBox(height: 15),
               _categoryButton("Dresses", "imgs/dress.jpeg"),
+              SizedBox(height: 15),
               _categoryButton("Skirts", "imgs/skirt.jpeg"),
+              SizedBox(height: 15),
               _categoryButton("T-shirts", "imgs/tshirt.jpeg"),
+              SizedBox(height: 15),
             ],
           ),
         ),
@@ -26,31 +68,49 @@ class WomenensClothingCat extends StatelessWidget {
   }
 
   Widget _categoryButton(String categoryName, String imagePath) {
-    return Container(
-      width: double.infinity,
-      child: ElevatedButton(
-        onPressed: () {
-          // Handle button press
-        },
-        style: ElevatedButton.styleFrom(
-          padding: EdgeInsets.all(16.0),
-          backgroundColor: Colors.white, // Set button background color
+    if (categoryName.toLowerCase().contains(searchText.toLowerCase()) || searchText.isEmpty) {
+      return Container(
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () {
+            // Handle button press
+          },
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.all(16.0),
+            backgroundColor: Colors.white, // Set button background color
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, // Align children to start and end
+            crossAxisAlignment: CrossAxisAlignment.center, // Align children vertically centered
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text(
+                    categoryName,
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0), // Adjust the right padding as needed
+              ),
+              Image.asset(
+                imagePath,
+                width: 100, // Adjust image width as needed
+                height: 100, // Adjust image height as needed
+              ),
+            ],
+          ),
         ),
-        child: Row(
-          children: [
-            Image.asset(
-              imagePath,
-              width: 100, // Adjust image width as needed
-              height: 100, // Adjust image height as needed
-            ),
-            SizedBox(width: 100), // Add some spacing between image and text
-            Text(
-              categoryName,
-              style: TextStyle(fontSize: 20, color: Colors.black),
-            ),
-          ],
-        ),
-      ),
-    );
+      );
+    } else {
+      return SizedBox(); // Return an empty SizedBox if the category doesn't match the search
+    }
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: WomenensClothingCat(),
+  ));
 }
